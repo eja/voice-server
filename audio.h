eaa88767825ddee306b75880833cc8ead5af6cc6
Ubaldo Porcheddu <ubaldo@eja.it>
2025-04-25 12:22:54 +0100

    Update audio.h

--- audio.h
+++ audio.h
@@ -1,10 +1,5 @@
 /* Copyright (C) 2025 by Ubaldo Porcheddu <ubaldo@eja.it> */
 
-#ifndef AUDIO_H
-#define AUDIO_H
-
 #import <Foundation/Foundation.h>
 
 BOOL convertAiffToWav(NSURL* sourceURL, NSURL* destinationURL, NSError** error);
-
-#endif
